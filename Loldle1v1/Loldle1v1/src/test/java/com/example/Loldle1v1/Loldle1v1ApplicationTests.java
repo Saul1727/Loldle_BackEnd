f7a838499c484543d5e674f4c471474b35860cc6
Sawe <149082520+Saul1727@users.noreply.github.com>
2025-10-23 11:28:47 +0200

    Add files via upload
    primer Commit

---
+++ Loldle1v1/Loldle1v1/src/test/java/com/example/Loldle1v1/Loldle1v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loldle1v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Loldle1v1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
